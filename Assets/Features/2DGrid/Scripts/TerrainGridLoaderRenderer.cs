615aee90628a0d8cdc648fca615503bc97338381
ahmedrazarana <ahmed.raza@hazelmobile.co>
2024-05-08 15:00:55 +0500

    feature: folder changes

---
+++ Assets/Features/2DGrid/Scripts/TerrainGridLoaderRenderer.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Features._2DGrid.Scripts
+{
+    // Main terrain grid loader and renderer component
+    public class TerrainGridLoaderRenderer : MonoBehaviour
+    {
+        public string jsonFileName; // Assign the path to your JSON file in the Unity Editor
+        public GameObject[] tilePrefabs; // Array of prefabs representing different tile types
+
+        public float tileSize;
+        public Quaternion tileRotation;
+
+        private ITerrainGridLoader _terrainGridLoader;
+        private ITerrainGridRenderer _terrainGridRenderer;
+        public TerrainData terrainGrid;
+
+        private void Start()
+        {
+            _terrainGridLoader = new JsonTerrainGridLoader();
+            _terrainGridRenderer = new TerrainGridRenderer();
+
+            // Load the JSON file from the Resources folder
+            var jsonFile = Resources.Load<TextAsset>(jsonFileName);
+            if (jsonFile != null)
+            {
+                terrainGrid = _terrainGridLoader.LoadTerrainGrid(jsonFile.text);
+                _terrainGridRenderer.RenderTerrainGrid(terrainGrid, tilePrefabs, transform, tileSize);
+            }
+            else
+            {
+                Debug.LogError("JSON file not found in Resources folder: " + jsonFileName);
+            }
+        }
+    }
+
+// Data classes representing the terrain grid
+    [System.Serializable]
+    public class TerrainData
+    {
+        public List<List<Tile>> TerrainGrid { get; set; }
+    }
+
+    [System.Serializable]
+    public class Tile
+    {
+        public int TileType { get; set; }
+    }
+}
---
+++ Assets/Features/2DGrid/Scripts/TerrainGridRenderer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Features._2DGrid.Scripts
+{
+    // Interface for terrain grid renderers
+    public interface ITerrainGridRenderer
+    {
+        void RenderTerrainGrid(TerrainData terrainGrid, GameObject[] tilePrefabs, Transform parentTransform, float tileSize);
+    }
+
+// Concrete implementation of terrain grid renderer
+    public class TerrainGridRenderer : ITerrainGridRenderer
+    {
+        public void RenderTerrainGrid(TerrainData terrainGrid, GameObject[] tilePrefabs, Transform parentTransform, float tileSize)
+        {
+            // Loop through the rows of the terrain grid
+            for (var i = 0; i < terrainGrid.TerrainGrid.Count; i++)
+            {
+                var row = terrainGrid.TerrainGrid[i];
+                var j = 0;
+                // Loop through the tiles in the row
+                foreach (var tile in row)
+                {
+                    var tileType = tile.TileType;
+                    var tilePrefab = tilePrefabs[tileType];
+                    Vector3 tilePosition = new Vector3(tileSize * j, 0, tileSize * -i);
+                    var tileInstance = Object.Instantiate(tilePrefab, tilePosition, Quaternion.Euler(90f, 0f, 0f), parentTransform);
+                    j++;
+                }
+            }
+        }
+    }
+}
