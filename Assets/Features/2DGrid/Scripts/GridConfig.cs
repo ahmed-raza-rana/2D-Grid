0ffb93a751bc522d50bdd5839565aa81907bec94
ahmedrazarana <ahmed.raza@hazelmobile.co>
2024-05-08 14:59:26 +0500

    scriptable: grid system

---
+++ Assets/Features/2DGrid/Scripts/GridConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Features._2DGrid.Scripts
+{
+    [CreateAssetMenu(fileName = "2DGridConfig",menuName = "2dGrid/GridConfig")]
+    public class GridConfig:ScriptableObject
+    {
+        public GameObject gridObj;
+        
+        public Sprite dirt; 
+        public Sprite grass;
+        public Sprite stone;
+        public Sprite wood;
+    }
+ 
+    [Serializable]
+    public enum TileEnum
+    {
+        Dirt = 0,
+        Grass = 1,
+        Stone = 2,
+        Wood = 3
+    }
+}
